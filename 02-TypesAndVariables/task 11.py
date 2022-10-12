41c2fb5883beba3dddaaa4855ffbf80c6bb1edb3
Santtu Körkkö <905622.student.uek.krakow.pl>
2022-10-12 10:40:29 +0200

    Commit message

---
+++ 02-TypesAndVariables/task 11.py
@@ -0,0 +1,4 @@
+name=input("Your name is: ")
+age=int(input("Your age: "))
+height=int(input("Your height: "))
+print(f"My name is {name}, I'm {age} years old, and my height is {height} cm")
---
+++ 02-TypesAndVariables/task 9.py
@@ -0,0 +1,3 @@
+name=input("Your Name ")
+surname=input("Your Surname ")
+print(f"My name is name is {name} {surname}")
